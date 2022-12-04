24ad52349843b951b39120304493fe81944794b7
singh7887 <67026777+singh7887@users.noreply.github.com>
2022-12-04 09:27:10 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ const navBtn = document.getElementById('nav-btn');
 const cancelBtn = document.getElementById('cancel-btn');
 const sideNav = document.getElementById('sidenav');
 const modal = document.getElementById('modal');
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
 
 navBtn.addEventListener("click", function(){
     sideNav.classList.add('show');
